9f8a0064142d5ce0ee9eb1196f37f1defdf2d020
Ravin66 <51367103+Ravin66@users.noreply.github.com>
2025-06-11 19:41:54 +1000

    Update version.go

--- version/version.go
+++ version/version.go
@@ -6,7 +6,7 @@ package version
 import "github.com/hashicorp/packer-plugin-sdk/version"
 
 var (
-	Version           = "0.1.3"
+	Version           = "0.1.4"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 	PluginVersion     = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
